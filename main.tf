41624f046e5d9664d1af885c5766c1ff21208d9e
kenneth-woo <145588338+kenneth-woo@users.noreply.github.com>
2023-09-20 16:24:16 -0400

    Create dev vpc 2.0

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -44,7 +44,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.public_subnets[0]
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
